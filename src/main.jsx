f28ee61677c0c868c7574b1f5135c805dcd5ce55
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-09-13 17:33:20 -0500

    feat(root): Ruta principal
    Donde se hace la logica para el correcto renderizado en el main - > root

---
+++ src/main.jsx
@@ -0,0 +1,63 @@
+// react dependences
+import React from 'react'
+import ReactDOM from 'react-dom/client'; 
+  // components
+import Root, {loader as rootLoader, action as rootAction } from './routes/root';
+import { loader as contactLoader, action as contactAction,} from './routes/contact';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+import EditContact, {action as editAction} from './routes/edit';
+import { action as destroyAction} from './routes/destroy';
+import Index from './routes';
+
+// routes 
+import { 
+  createBrowserRouter,
+   RouterProvider 
+} from 'react-router-dom';
+
+// styles 
+import "./index.css"
+
+const rootRouter = createBrowserRouter([
+   {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage/>,
+      loader: rootLoader,
+      action: rootAction,
+      children: [
+        {
+          errorElement: <ErrorPage />,
+          children: [
+            {index: true, element: <Index/>},
+            {
+              path: "contacts/:contactId",
+              element: <Contact />,
+              loader: contactLoader,
+              action: contactAction
+            },
+            {
+              path: "contacts/:contactId/edit",
+              element: <EditContact />,
+              loader: contactLoader,
+              action: editAction
+            },
+            {
+              path: "contacts/:contactId/destroy",
+              action: destroyAction,
+              errorElement: <div>Oops! There was an error.</div>,
+            },
+          ]
+        }
+        
+      ]
+   }
+])
+
+// render
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>  
+    <RouterProvider router={rootRouter}/>
+  </React.StrictMode>,
+)
